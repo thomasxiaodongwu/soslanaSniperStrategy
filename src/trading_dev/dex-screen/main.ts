a4ce5d1d1a93d3841530968a64ccbb48fd212c41
thomasxiaodongwu <755147511@qq.com>
2024-10-28 19:35:53 +0800

    change

--- src/trading_dev/dex-screen/main.ts
+++ src/trading_dev/dex-screen/main.ts
@@ -1,27 +1,36 @@
 import { logger } from "../../helpers/logger";
 import axios from "axios";
 import { Worker } from 'worker_threads';
+import path from 'path';
 
-const worker = new Worker('./worker.ts');
-worker.on('message', (msg) => {
-    console.log('来自工作线程的消息:', msg);
+const worker = new Worker(path.resolve(__dirname, './worker.ts'));
+
+worker.on('error', (error) => {
+    console.error(`Worker error: ${error}`);
+});
+
+worker.on('exit', (code) => {
+    if (code !== 0) {
+        console.error(`Worker stopped with exit code ${code}`);
+    } else {
+        console.log('Worker exited successfully');
+    }
 });
 
 const PING_INTERVAL_MS = 1_001;
-async function snipe() {
 
-    setInterval(async () => {
-        axios.get("https://api.dexscreener.com/token-profiles/latest/v1", {})
-            .then(response => {
-                const resJson = JSON.parse(response.data);
-                if(resJson.chainId === "solana"){
-                    worker.postMessage({ computePrimesUpTo: resJson.tokenAddress });
-                }
-            })
-            .catch(error => {
-                logger.info(error);
-            });
-    }, PING_INTERVAL_MS);
-}
 
-snipe();
+setInterval(async () => {
+    axios.get("https://api.dexscreener.com/token-profiles/latest/v1", {})
+        .then(response => {
+            const resJson = JSON.parse(response.data);
+            if(resJson.chainId === "solana"){
+                worker.postMessage({ computePrimesUpTo: resJson.tokenAddress });
+            }
+        })
+        .catch(error => {
+            logger.info(error);
+        });
+}, PING_INTERVAL_MS);
+
+
